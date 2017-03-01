5d3347b33bd9df9c557ed00942c229b8812b7f80
Nael Khan <naelkhann@gmail.com>
2017-03-01 17:08:32 -0500

    Setup project with main dependencies, generate Easel object

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './lib/descent.js',
+  output: {
+    filename: './lib/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["*", ".js"]
+  }
+}
